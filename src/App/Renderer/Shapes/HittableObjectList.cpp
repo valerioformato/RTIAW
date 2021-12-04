1263244fc4b9bd3fdeb6a42e3ef0cc253136833a
Valerio Formato <valerio.formato@cern.ch>
2021-12-04 11:13:39 +0100

    Fix unfitting usage of accumulate in HittableObjectList::Hit

--- src/App/Renderer/Shapes/HittableObjectList.cpp
+++ src/App/Renderer/Shapes/HittableObjectList.cpp
@@ -6,13 +6,15 @@ namespace RTIAW::Render {
 std::optional<HitRecord> HittableObjectList::Hit(const Ray &r, float t_min, float t_max) const {
   float closest_so_far = t_max;
 
-  return std::accumulate(begin(objects), end(objects), std::optional<HitRecord>{},
-                         [&](const auto &temp_value, const auto &object) {
-                           if (auto temp_hit = object->Hit(r, t_min, closest_so_far); temp_hit) {
-                             closest_so_far = temp_hit.value().t;
-                             return temp_hit;
-                           }
-                           return temp_value;
-                         });
+  std::optional<HitRecord> result{};
+
+  for (const auto &object : objects) {
+    if (auto temp_hit = object->Hit(r, t_min, closest_so_far); temp_hit) {
+      closest_so_far = temp_hit.value().t;
+      result = temp_hit;
+    }
+  }
+
+  return result;
 }
 } // namespace RTIAW::Render
