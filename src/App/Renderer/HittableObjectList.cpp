46df3a037ab6e2cf1ec1ee3279f5fba76912b5b8
Lukas Miething <lukas.miething@web.de>
2021-12-12 12:44:16 +0100

    store all spheres in one list, only compute the material for  the closest sphere

--- src/App/Renderer/HittableObjectList.cpp
+++ src/App/Renderer/HittableObjectList.cpp
@@ -5,57 +5,42 @@
 namespace RTIAW::Render {
 std::optional<ScatteringRecord> HittableObjectList::Hit(const Ray &r, float t_min, float t_max) const {
   float closest_so_far = t_max;
+  size_t closest_sphere_idx = -1;
+  for (size_t sphere_idx = 0; sphere_idx < m_spheres.size(); ++sphere_idx) {
+    const auto &sphere = m_spheres[sphere_idx];
+    const vec3 oc = r.m_origin - sphere.m_center;
+    const float half_b = glm::dot(oc, r.m_unit_direction);
 
-  std::optional<ScatteringRecord> result;
+    const float discriminant = half_b * half_b - glm::sq_length(oc) + sphere.m_radius * sphere.m_radius;
+    if (discriminant < 0.f)
+      continue;
+    const float sqrtd = sqrt(discriminant);
 
-  const auto hit_spheres = [&](const auto &spheres) -> size_t {
-    size_t closest_sphere_idx = -1;
-    for (size_t sphere_idx = 0; sphere_idx < spheres.size(); ++sphere_idx) {
-      const auto &sphere = spheres[sphere_idx];
-      const vec3 oc = r.m_origin - sphere.m_center;
-      const float half_b = glm::dot(oc, r.m_unit_direction);
-
-      const float discriminant =
-          half_b * half_b - glm::sq_length(oc) + sphere.m_radius * sphere.m_radius;
-      if (discriminant < 0.f)
+    // Find the nearest root that lies in the acceptable range.
+    float root = -half_b - sqrtd;
+    if (root < t_min || t_max < root) {
+      root = -half_b + sqrtd;
+      if (root < t_min || t_max < root)
         continue;
-      const float sqrtd = sqrt(discriminant);
-
-      // Find the nearest root that lies in the acceptable range.
-      float root = -half_b - sqrtd;
-      if (root < t_min || t_max < root) {
-        root = -half_b + sqrtd;
-        if (root < t_min || t_max < root)
-          continue;
-      }
-
-      if (root < closest_so_far) {
-        closest_so_far = root;
-        closest_sphere_idx = sphere_idx;
-      }
     }
-    return closest_sphere_idx;
-  };
 
-
-  const auto lambertian_idx = hit_spheres(m_lambertian_spheres);
-  if (lambertian_idx != size_t(-1)) {
-    const auto record = m_lambertian_spheres[lambertian_idx].GetHitRecord(closest_so_far, r);
-    result = m_lambertian_materials[lambertian_idx].Scatter(r, record);
+    if (root < closest_so_far) {
+      closest_so_far = root;
+      closest_sphere_idx = sphere_idx;
+    }
   }
 
-  const auto dielectric_idx = hit_spheres(m_dielectric_spheres);
-  if (dielectric_idx != size_t(-1)) {
-    const auto record = m_dielectric_spheres[dielectric_idx].GetHitRecord(closest_so_far, r);
-    result = m_dielectric_materials[dielectric_idx].Scatter(r, record);
-  }
+  if (closest_sphere_idx == size_t(-1))
+    return {};
 
-  const auto metal_idx = hit_spheres(m_metal_spheres);
-  if (metal_idx != size_t(-1)) {
-    const auto record = m_metal_spheres[metal_idx].GetHitRecord(closest_so_far, r);
-    result = m_metal_materials[metal_idx].Scatter(r, record);
-  }
+  const auto record = m_spheres[closest_sphere_idx].GetHitRecord(closest_so_far, r);
 
-  return result;
+  if (closest_sphere_idx >= m_lambertian_offset)
+    return m_lambertian_materials[closest_sphere_idx - m_lambertian_offset].Scatter(r, record);
+
+  if (closest_sphere_idx >= m_metal_offset)
+    return m_metal_materials[closest_sphere_idx - m_metal_offset].Scatter(r, record);
+
+  return m_dielectric_materials[closest_sphere_idx].Scatter(r, record);
 }
 } // namespace RTIAW::Render
--- src/App/Renderer/HittableObjectList.h
+++ src/App/Renderer/HittableObjectList.h
@@ -17,35 +17,39 @@ public:
   HittableObjectList() = default;
 
   inline void Clear() {
-    m_dielectric_spheres.clear();
-    m_lambertian_spheres.clear();
-    m_metal_spheres.clear();
+    m_spheres.clear();
+    m_metal_offset = m_lambertian_offset = 0;
     m_dielectric_materials.clear();
-    m_lambertian_materials.clear();
     m_metal_materials.clear();
+    m_lambertian_materials.clear();
   }
 
   inline void Add(Shapes::Sphere sphere, Materials::Dielectric material) {
-    m_dielectric_spheres.push_back(sphere);
+    m_spheres.insert(m_spheres.begin() + m_metal_offset, sphere);
     m_dielectric_materials.push_back(material);
-  }
-  inline void Add(Shapes::Sphere sphere, Materials::Lambertian material) {
-    m_lambertian_spheres.push_back(sphere);
-    m_lambertian_materials.push_back(material);
+    ++m_metal_offset;
+    ++m_lambertian_offset;
   }
   inline void Add(Shapes::Sphere sphere, Materials::Metal material) {
-    m_metal_spheres.push_back(sphere);
+    m_spheres.insert(m_spheres.begin() + m_lambertian_offset, sphere);
     m_metal_materials.push_back(material);
+    ++m_lambertian_offset;
+  }
+  inline void Add(Shapes::Sphere sphere, Materials::Lambertian material) {
+    m_spheres.push_back(sphere);
+    m_lambertian_materials.push_back(material);
   }
   [[nodiscard]] std::optional<ScatteringRecord> Hit(const Ray &r, float t_min, float t_max) const;
 
 private:
-  std::vector<Shapes::Sphere> m_dielectric_spheres;
-  std::vector<Shapes::Sphere> m_lambertian_spheres;
-  std::vector<Shapes::Sphere> m_metal_spheres;
+  // enum class MaterialType : char { DIELECTRIC, LAMBERTIAN, METAL };
+
+  std::vector<Shapes::Sphere> m_spheres;
+  size_t m_metal_offset = 0;
+  size_t m_lambertian_offset = 0;
   std::vector<Materials::Dielectric> m_dielectric_materials;
-  std::vector<Materials::Lambertian> m_lambertian_materials;
   std::vector<Materials::Metal> m_metal_materials;
+  std::vector<Materials::Lambertian> m_lambertian_materials;
 };
 } // namespace RTIAW::Render
 
